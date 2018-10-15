009bab6318061c48d1e7c91e2911ec340e9201a4
Michael Vietluzhskykh <mishavetl@gmail.com>
2018-10-15 17:00:04 +0300

    add string_to_string function

--- src/std/string.cpp
+++ src/std/string.cpp
@@ -21,6 +21,11 @@ int integral_to_string_func(context_t& context, const std::vector<value_t>& args
 	return 0;
 }
 
+int string_to_string_func(context_t& context, const std::vector<value_t>& args, value_t& ret) {
+	ret.data.string = context.gc.add_r(new std::string{ *args[0].data.string });
+	return 0;
+}
+
 void register_string(context_t& context) {
 	value_t real_to_string = { { type_id::function, { { type_id::real } } } };
 	real_to_string.data.function = real_to_string_func;
@@ -29,7 +34,11 @@ void register_string(context_t& context) {
 	value_t integral_to_string = { { type_id::function, { { type_id::integral } } } };
 	integral_to_string.data.function = integral_to_string_func;
 	add(context, "string", integral_to_string);
-	
+
+	value_t string_to_string = { { type_id::function, { { type_id::string } } } };
+	string_to_string.data.function = string_to_string_func;
+	add(context, "string", string_to_string);
+
 	//value_t plus = { { { type_id::function },{ { type_id::real },{ type_id::real } } } };
 	//plus.data.function = real_plus_func;
 	//add(context_t, "+", plus);
